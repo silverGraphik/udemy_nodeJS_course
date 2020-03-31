cfd4762b88995d83ca9d4832ade5ef1fb33aef37
Gaetan Gerard <gaetan.gerard@data-essential.com>
2020-03-31 17:27:06 +0200

    Complete Section 09

--- app.js
+++ app.js
@@ -1,22 +1,19 @@
-
 const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
 
-// const expressHbs = require('express-handlebars');
-
 const app = express();
 
-// app.engine('hbs', expressHbs({layoutsDir: 'views/layouts', defaultLayout: 'main-layout', extname: 'hbs'})); //Init express-handlebars template engine
-app.set('view engine', 'ejs'); // set witch template engine you want to use
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
